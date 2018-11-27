0727095b957330b2d34c5bca561cdffe63073b72
raphael <raphinha_31@hotmail.com>
2018-11-27 23:06:07 +0000

    atualizada a makefile e pasta source

---
+++ Trabalho/src/src/bucketSort.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctime>
+
+void geraInputs(int numerosInput[],int sizeInput);
+void criarBuckets(int contadores[], int nBuckets);
+void insereBuckets(int buckets[], int contadores[], int numerosInput[],int sizeInput);
+void ordenaBuckets(int buckets[], int contadores[], int sizeInput, int nBuckets);
+void ordenaInput(int buckets[],int contadores[], int numerosInput[], int sizeInput, int nBuckets);
+void bucketSortSeq(int numerosInput[], int sizeInput);
+void printArray(int array[], int sizeArray);
